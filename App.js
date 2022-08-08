10f3218f426cc0f29691134300d317d4e2d4f7b3
tejas-gulhane <103639207+tejas-gulhane@users.noreply.github.com>
2022-08-08 16:56:57 +0530

    M4_task5

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import  React ,{ Fragment } from "react";
 import Header from "./component/Layout/Header"
 import MealSummary from "./component/Meals/MealSummary";
 import Meal from "./component/Meals/Meal";
+import Cart from "./component/Cart/Cart"
 
 function App() {
   return (
     <>
+    <Cart />
       <Header />
       <MealSummary />
       <Meal />
