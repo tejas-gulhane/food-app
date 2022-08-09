42dfbad05234bc8d8a0d04a4a523c8625631e23e
tejas-gulhane <103639207+tejas-gulhane@users.noreply.github.com>
2022-08-09 10:35:13 +0530

    M4_task6

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
-import  React ,{ Fragment } from "react";
-import Header from "./component/Layout/Header"
-import MealSummary from "./component/Meals/MealSummary";
-import Meal from "./component/Meals/Meal";
-import Cart from "./component/Cart/Cart"
+import React, { Fragment, useState } from "react";
+import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meal";
+import Cart from './component/Cart/Cart';
 
 function App() {
-  return (
-    <>
-    <Cart />
-      <Header />
-      <MealSummary />
-      <Meal />
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-    </>
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals/>
+      </main>
+    </Fragment>
   );
-}
+} 
 export default App;
